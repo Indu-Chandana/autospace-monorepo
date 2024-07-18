9f006f4e7db4c6a32a6c148bbcfa7ad4aae5a388
Indu-Chandana <indu.99.ic@gmail.com>
2024-07-18 05:36:20 +0530

    July18 001 start graphQL and started basic CRUD operation.

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { GraphQLModule } from '@nestjs/graphql'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import { join } from 'path'
+import { ConfigModule } from '@nestjs/config'
+import { UsersModule } from './users/users.module'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      introspection: true, // some times client may want to get schema info.
+      fieldResolverEnhancers: ['guards'], // to protect resolvers- we have to enable this.
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'), // from the current dir -> we are going to 'src/schema.gql'.
+      // when ever nest server runs, It will generate graphQl schema
+      buildSchemaOptions: {
+        numberScalarMode: 'integer', // by default it is float. we modifing into int
+      },
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
