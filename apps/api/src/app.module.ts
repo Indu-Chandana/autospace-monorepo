73cc558b350b4d24b1cd0871cda8f5aa3686c84d
Indu-Chandana <indu.99.ic@gmail.com>
2024-07-24 06:13:32 +0530

    July24 001 graphql and rest API both work in userModule.

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,11 +5,21 @@ import { GraphQLModule } from '@nestjs/graphql'
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { join } from 'path'
 import { ConfigModule } from '@nestjs/config'
-import { UsersModule } from './users/users.module'
+import { PrismaModule } from './common/prisma/prisma.module'
+import { UsersModule } from './models/users/users.module'
+import { JwtModule } from '@nestjs/jwt'
+
+// Todo: move this to util lib because this val also need to access in FE
+const MAX_AGE = 24 * 60 * 60
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: MAX_AGE },
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       introspection: true, // some times client may want to get schema info.
@@ -20,6 +30,7 @@ import { UsersModule } from './users/users.module'
         numberScalarMode: 'integer', // by default it is float. we modifing into int
       },
     }),
+    PrismaModule,
     UsersModule,
   ],
   controllers: [AppController],
--- apps/api/src/users/entities/user.entity.ts
+++
@@ -1,7 +0,0 @@
-import { ObjectType } from '@nestjs/graphql'
-
-@ObjectType()
-export class User {
-  // @Field() -- do not need to use decorator because we added CLI plugin (nest-cli.json)
-  uid: string
-}
