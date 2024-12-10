eef11a0e36539955944fab216a2f307ab4433cc8
Indu-Chandana <indu.99.ic@gmail.com>
2024-12-10 10:41:15 +0530

    Dec10 (2) stripe success fail redirections done.

--- apps/api/src/models/stripe/stripe.controller.ts
+++ apps/api/src/models/stripe/stripe.controller.ts
@@ -1,24 +1,24 @@
 import {
-  // BadRequestException,
+  BadRequestException,
   Body,
   Controller,
   Get,
   Post,
-  // Query,
-  // Res,
+  Query,
+  Res,
 } from '@nestjs/common'
 import { BookingsService } from '../bookings/graphql/bookings.service'
 import StripeService from './stripe.service'
 import { CreateStripeDto } from './dto/create-stripe-session.dto'
-// import { CreateBookingInput } from '../bookings/graphql/dtos/create-booking.input'
-// import { Response } from 'express'
+import { CreateBookingInput } from '../bookings/graphql/dtos/create-booking.input'
+import { Response } from 'express'
 
 @Controller('stripe')
 export class StripeController {
   constructor(
     private readonly stripeService: StripeService,
     private readonly bookingService: BookingsService,
-  ) {}
+  ) { }
 
   @Get()
   helloStripe() {
@@ -30,23 +30,26 @@ export class StripeController {
     return this.stripeService.createStripeSession(createStripeDto)
   }
 
-  // @Get('success')
-  // async handleStripeSuccess(
-  //   @Query('session_id') sessionId: string,
-  //   @Res() res: Response,
-  // ) {
-  //   if (!sessionId) {
-  //     throw new BadRequestException('Session id missing.')
-  //   }
+  @Get('success')
+  async handleStripeSuccess(
+    @Query('session_id') sessionId: string,
+    @Res() res: Response,
+  ) {
+    if (!sessionId) {
+      throw new BadRequestException('Session id missing.')
+    }
 
-  //   const session =
-  //     await this.stripeService.stripe.checkout.sessions.retrieve(sessionId)
-  //   const { uid, bookingData } = session.metadata
+    const session =
+      await this.stripeService.stripe.checkout.sessions.retrieve(sessionId)
+    const {
+      // uid, 
+      bookingData } = session.metadata
 
-  //   const bookingInput: CreateBookingInput = JSON.parse(bookingData)
+    const bookingInput: CreateBookingInput = JSON.parse(bookingData)
 
-  //   const newBooking = await this.bookingService.create(bookingInput)
+    // const newBooking = 
+    await this.bookingService.create(bookingInput)
 
-  //   res.redirect(process.env.BOOKINGS_REDIRECT_URL)
-  // }
+    res.redirect(process.env.BOOKINGS_REDIRECT_URL)
+  }
 }
---
+++ apps/web/src/app/booking-failed/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const page = () => {
+  return <div>Booking failed</div>
+}
+
+export default page
