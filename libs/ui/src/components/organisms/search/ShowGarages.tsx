cf50508f23d26750132e8ea96f7d8350386ae76a
Indu-Chandana <indu.99.ic@gmail.com>
2024-12-10 11:21:23 +0530

    Dec10 (3) Garage loarding and no Garages alert UI

--- libs/ui/src/components/organisms/search/ShowGarages.tsx
+++ libs/ui/src/components/organisms/search/ShowGarages.tsx
@@ -5,10 +5,13 @@ import { useEffect } from 'react'
 import { GarageMarker } from './GarageMarker'
 // import { FormTypeSearchGarage } from '@autospace/forms/src/searchGarages'
 import { useConvertSearchFormToVariables } from '@autospace/forms/src/adapters/searchFormAdapter'
+import { Panel } from '../map/Panel'
+import { Loader } from '../../molecules/Loader'
+import { IconInfoCircle } from '@tabler/icons-react'
 
 export const ShowGarages = () => {
     const [searchGarages, {
-        //loading,
+        loading,
         data,
         // error
     }] = useLazyQuery(SearchGaragesDocument)
@@ -30,8 +33,24 @@ export const ShowGarages = () => {
         // }, [end, locationFilter, searchGarages, start])
     }, [searchGarages, variables])
 
+    if (data?.searchGarages.length === 0) {
+        return (
+            <Panel position='center-center'
+                className='bg-white/50 shadow border-white border backdrop-blur-sm'
+            >
+                <div className='flex items-center justify-center gap-2'>
+                    <IconInfoCircle /> <div>No Parking slots found in this area.</div>
+                </div>
+            </Panel>
+        )
+    }
     return (
         <>
+            {loading ? (
+                <Panel position='center-bottom'>
+                    <Loader />
+                </Panel>
+            ) : null}
             {data?.searchGarages.map((garage) => (
                 <GarageMarker key={garage.id} marker={garage} />
             ))}
