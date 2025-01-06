91e93439a1df229cd3ab60aea6c4bdf65f856d45
Indu-Chandana <indu.99.ic@gmail.com>
2025-01-06 16:37:04 +0530

    Jan06 (1) BUG Next Version conflict. all apps need to  stick to same version

--- apps/api/src/models/stripe/stripe.service.ts
+++ apps/api/src/models/stripe/stripe.service.ts
@@ -10,7 +10,7 @@ export default class StripeService {
 
   constructor() {
     this.stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-      apiVersion: '2024-11-20.acacia',
+      apiVersion: '2024-12-18.acacia', // '2024-11-20.acacia',
     })
   }
 
